3ded203fc97e40721f0b3baa88f6c41a6dae1a50
hiennv94 <hiennv.th@gmail.com>
2017-04-02 12:39:25 +0700

    scroll for header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { AppState } from './app.service';
 
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    './app.component.css'
+    './app.component.scss'
   ],
   templateUrl: './app.component.html'
 })
--- client/src/app/common/header/header.component.ts
+++ client/src/app/common/header/header.component.ts
@@ -1,40 +1,64 @@
 import {
   Component,
   OnInit,
-  HostListener
+  HostListener,
+  ElementRef,
+  AfterViewInit
 } from '@angular/core';
 
 import { DOCUMENT } from '@angular/platform-browser';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 
-export class Header implements OnInit {
+export class Header implements AfterViewInit {
   public angularclassLogo = 'assets/img/cd-logo.svg';
   public scrolling = false;
+  public headerHeight;
+  public previousTop = 0;
+  public currentTop = 0;
+  public scrollDelta = 10;
+  public scrollOffset = 150;
+  public scrollCheck = '';
 
-  constructor() {
+  constructor(private el: ElementRef) {}
 
-  }
+  public ngAfterViewInit() {}
 
-  public ngOnInit() {
-
-  }
-
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-    console.log("scrolling")
+  @HostListener('window:scroll', [])onWindowScroll() {
+    var headerTest = this;
     if( !this.scrolling ) {
-			this.scrolling = true;
-			(!window.requestAnimationFrame)
-				? setTimeout(this.autoHideHeader, 250)
-				: requestAnimationFrame(this.autoHideHeader);
-		}
+      this.scrolling = true;
+      if (!window.requestAnimationFrame) {
+        setTimeout(function() {
+          this.autoHideHeader(headerTest);
+        }.bind(headerTest), 250);
+      } else {
+        requestAnimationFrame(function() {
+          this.autoHideHeader(headerTest);
+        }.bind(headerTest));
+      }
+    }
   }
 
-  public autoHideHeader() {
+  public autoHideHeader(header) {
+    var currentTop = $(window).scrollTop();
+    header.checkSimpleNavigation(currentTop, header);
+    header.previousTop = currentTop;
+    header.scrolling = false;
+  }
 
+  public checkSimpleNavigation(currentTop, header) {
+    header.currentTop = currentTop;
+    if (header.previousTop - header.currentTop > header.scrollDelta) {
+      header.scrollCheck ='';
+    } else if((header.currentTop - header.previousTop > header.scrollDelta) &&
+      (header.currentTop > header.scrollOffset)) {
+        header.scrollCheck ='is-hidden';
+    }
   }
 }
