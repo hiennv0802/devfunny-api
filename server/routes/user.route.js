25d2d0b3c9448f512ff16bcb4e5518b54724d397
hienbx94 <hienbx94@gmail.com>
2017-09-01 13:47:32 +0700

    add doc for signup with email

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -8,35 +8,6 @@ const router = express.Router();
 router.route('/')
   .get(userCtrl.list)
 
-/**
- * @api {post} /api/users Create new user
- * @apiVersion 1.0.0
- * @apiGroup User
- * @apiName CreateUser
- *
- * @apiParam {String} email Email of user.
- * @apiParam {String} password Password of user.
- *
- * @apiSuccess {String} id ID of user.
- * @apiSuccess {String} name Name of user.
- * @apiSuccess {String} email Email of user.
- * @apiSuccessExample {json} Success-Response:
- *  {
- *    "__v": 0,
- *    "email": "hienbx94@gmail.com",
- *    "password": "$2a$10$ZQiDyDq92YBYCfe4sN/8G.sp5fUGKNECWOd/X0CPg3eMzZMFdfSi6",
- *    "_id": "593137c353afe608c64d800f",
- *    "createdAt": "2017-06-02T10:02:43.475Z"
- *  }
- *
- * @apiError InternalServerError InternalServerError
- * @apiErrorExample {json} Error-Response:
- *   {
- *     "message": "Internal Server Error",
- *      "stack": {}
- *   }
- */
-
   .post(validate(paramValidation.createUser), userCtrl.create);
 
 router.route('/:userId')
