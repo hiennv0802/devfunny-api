49f86d1f3c52492a5536c624da23271f945f833d
oBuiXuanHien <bui.xuan.hien@framgia.com>
2018-03-27 17:18:19 +0700

    eslint fix new-cap

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,15 +5,15 @@ import paramValidation from '../../config/lib/param-validation';
 import authCtrl from '../controllers/auth.controller';
 import config from '../../config/config';
 
-const router = express.Router(); // eslint-disable-line new-cap
+const router = express.Router();
 
 /** POST /api/auth/login - Returns token if correct username and password is provided */
-router.route('/login')
-  .post(validate(paramValidation.login), authCtrl.login);
+router.route('/login').post(validate(paramValidation.login), authCtrl.login);
 
 /** GET /api/auth/random-number - Protected route,
  * needs token returned by the above as header. Authorization: Bearer {token} */
-router.route('/random-number')
+router
+  .route('/random-number')
   .get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
 
 export default router;
