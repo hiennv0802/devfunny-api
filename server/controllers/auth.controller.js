ae0439f525ea8c0ad6a190cde829f787c85619ee
oBuiXuanHien <bui.xuan.hien@framgia.com>
2018-03-27 17:33:02 +0700

    remove api unnecessary

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,52 +12,52 @@ import User from '../models/user.model';
  * @returns {*}
  */
 function login(req, res, next) {
-  User.findOne({
-    email: req.body.email
-  }, (errors, user) => {
-    if (errors) throw errors;
+  User.findOne(
+    {
+      email: req.body.email
+    },
+    (errors, user) => {
+      if (errors) throw errors;
 
-    if (!user) {
-      const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
-      return next(err);
-    }
-
-    return user.comparePassword(req.body.password, (err, isMatch) => {
-      if (err) throw err;
-
-      if (isMatch) {
-        const token = jwt.sign({
-          _id: user._id,
-          email: user.email,
-          username: user.username
-        }, config.jwtSecret);
-
-        return res.json({
-          token: token, // eslint-disable-line
-          username: user.username,
-          email: user.email,
-          social_type: user.social_type,
-          social_id: user.social_id
-        });
+      if (!user) {
+        const err = new APIError(
+          'Authentication error',
+          httpStatus.UNAUTHORIZED,
+          true
+        );
+        return next(err);
       }
-      const errAuth = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
-      return next(errAuth);
-    });
-  });
+
+      return user.comparePassword(req.body.password, (err, isMatch) => {
+        if (err) throw err;
+
+        if (isMatch) {
+          const token = jwt.sign(
+            {
+              _id: user._id,
+              email: user.email,
+              username: user.username
+            },
+            config.jwtSecret
+          );
+
+          return res.json({
+            token: token, // eslint-disable-line
+            username: user.username,
+            email: user.email,
+            social_type: user.social_type,
+            social_id: user.social_id
+          });
+        }
+        const errAuth = new APIError(
+          'Authentication error',
+          httpStatus.UNAUTHORIZED,
+          true
+        );
+        return next(errAuth);
+      });
+    }
+  );
 }
 
-/**
- * This is a protected route. Will return random number only if jwt token is provided in header.
- * @param req
- * @param res
- * @returns {*}
- */
-function getRandomNumber(req, res) {
-  // req.user is assigned by jwt middleware if valid token is provided
-  return res.json({
-    user: req.user,
-    num: Math.random() * 100
-  });
-}
-
-export default { login, getRandomNumber };
+export default { login };
