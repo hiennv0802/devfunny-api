202addd7d833bbae432ddeacfa31365e2d366552
oBuiXuanHien <bui.xuan.hien@framgia.com>
2018-03-30 13:26:11 +0700

    remove comment

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,51 +5,42 @@ import config from '../../config/config';
 import User from '../models/user.model';
 
 function login(req, res, next) {
-  User.findOne(
-    {
-      email: req.body.email
-    },
-    (errors, user) => {
-      if (errors) throw errors;
+  User.findOne({ email: req.body.email }, (errors, user) => {
+    if (errors) throw errors;
 
-      if (!user) {
-        const err = new APIError(
-          'Authentication error',
-          httpStatus.UNAUTHORIZED,
-          true
-        );
-        return next(err);
-      }
-
-      return user.comparePassword(req.body.password, (err, isMatch) => {
-        if (err) throw err;
-
-        if (isMatch) {
-          const token = jwt.sign(
-            {
-              _id: user._id
-            },
-            config.jwtSecret,
-            { expiresIn: 86400 }
-          );
-
-          return res.json({
-            token: token, // eslint-disable-line
-            username: user.username,
-            email: user.email,
-            social_type: user.social_type,
-            social_id: user.social_id
-          });
-        }
-        const errAuth = new APIError(
-          'Authentication error',
-          httpStatus.UNAUTHORIZED,
-          true
-        );
-        return next(errAuth);
-      });
+    if (!user) {
+      const err = new APIError(
+        'Authentication error',
+        httpStatus.UNAUTHORIZED,
+        true
+      );
+      return next(err);
     }
-  );
+
+    return user.comparePassword(req.body.password, (err, isMatch) => {
+      if (err) throw err;
+
+      if (isMatch) {
+        const token = jwt.sign({ _id: user._id }, config.jwtSecret, {
+          expiresIn: 86400
+        });
+
+        return res.json({
+          token,
+          username: user.username,
+          email: user.email,
+          social_type: user.social_type,
+          social_id: user.social_id
+        });
+      }
+      const errAuth = new APIError(
+        'Authentication error',
+        httpStatus.UNAUTHORIZED,
+        true
+      );
+      return next(errAuth);
+    });
+  });
 }
 
 export default { login };
